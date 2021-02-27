ebf2d1a32266b1c678d4a4f074b5d546c932464d
Thestickman391 <stickman391@gmail.com>
2021-02-27 23:41:39 +1300

    REAL api

--- index.js
+++ index.js
@@ -1,37 +1,93 @@
-var ffmpeg = require('fluent-ffmpeg');
 const fs = require('fs');
-let express = require('express');
+const express = require('express');
 const app = express();
-// var cors = require('cors')
-// app.use(cors())
+const videoStitch = require('video-stitch');
+const videoConcat = videoStitch.concat;
 
-app.get('/api/', (req, res) => {
-  ffmpeg('videos/birthday01.mp4')
-    .input('videos/names/finn.mp4')
-    .input('videos/birthday02.mp4')
-    .input('videos/names/finn.mp4')
-    .input('videos/birthday03.mp4')
-    .on('error', function(err) {
-      console.log('An error occurred: ' + err.message);
-    })
-    .on('end', function() {
-      var base64video = fs.readFileSync('videos/out.mp4', {encoding: 'base64'});
-      const returnVideo = Buffer.from(base64video, 'base64');
-      res.setHeader('Content-Type', 'video/mp4');    
-      res.setHeader('Content-Length', returnVideo.length);
-      res.writeHead(200);
-      res.end(returnVideo); 
-      try {
-        fs.unlinkSync('videos/out.mp4');
-        //file removed
-      } catch(err) {
-        console.error(err);
-      }
-      return;
-    })
-    .mergeToFile('videos/out.mp4', 'videos');
-  });
+const cors = require('cors');
+app.use(cors());
+
+//startup stuff for reading directories and storing them lol
+const names = fs.readdirSync('videos/names');
+const heyNames = fs.readdirSync('videos/names/hey');
+
+var serveDir = "./serve";
+if (!fs.existsSync(serveDir)){
+	fs.mkdirSync(serveDir);
+}
+
+app.get('/', (req, res) => {
+	var clips = new Array();
+	var outputNameAsString = [req.query.type, req.query.name1, req.query.name2].filter(Boolean).join("_") + ".webm"; //we use .filter and .join instead of concatenate because name2 might not always exist
+	
+	console.log(outputNameAsString);
+
+	const varServedFiles = fs.readdirSync(serveDir);
+
+	if(varServedFiles.includes(outputNameAsString)) {
+		console.log("found video " + outputNameAsString + " in cache, serving");
+		res.set("Content-Disposition", "attachment;filename=" + outputNameAsString);
+		res.attachment(outputNameAsString);
+		res.sendFile(outputNameAsString, {
+			root: __dirname
+		});
+	} else {
+
+		switch (req.query.type) {
+			case "anniversary":
+				if (heyNames.includes(req.query.name1 + '.webm')) { //we always want anniversary videos to start with "hey" if we can
+					clips.push({"fileName": "videos/names/hey/" + req.query.name1 + ".webm"}); //rather than stripping every single file name in the array of the phrase ".webm", lets just compare with .webm added
+					clips.push({"fileName": "videos/and.webm"});
+					clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
+				} else if (heyNames.includes(req.query.name2 + '.webm')) {
+					clips.push({"fileName": "videos/names/hey/" + req.query.name2 + ".webm"});
+					clips.push({"fileName": "videos/and.webm"});
+					clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				} else {
+					clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+					clips.push({"fileName": "videos/and.webm"});
+					clips.push({"fileName": "videos/names/" + req.query.name2 + ".webm"});
+				}
+				clips.push({"fileName": "videos/anniversary.webm"});
+				break;
+
+			case "birthday_type01":
+				clips.push({"fileName": "videos/birthday_type01_01.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				clips.push({"fileName": "videos/birthday_type01_02.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				clips.push({"fileName": "videos/birthday_type01_03.webm"});
+				break;
+			
+			case "birthday_type02":
+				clips.push({"fileName": "videos/birthday_type02_01.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				clips.push({"fileName": "videos/birthday_type02_02.webm"});
+				clips.push({"fileName": "videos/names/" + req.query.name1 + ".webm"});
+				clips.push({"fileName": "videos/birthday_type02_03.webm"});
+				break;
+		}
+
+		// console.log(clips);
+		console.log("producing new video " + outputNameAsString);
+		videoConcat({
+			silent: true, // if set to false, gives detailed output on console
+			overwrite: true //lets keep this at true just on the off chance that checking if the file exists fails
+		})
+		.clips(clips)
+		.output("serve/" + outputNameAsString)
+		.concat()
+		.then((outputFileName) => {
+			console.log("produced new video " + outputNameAsString);
+			res.set("Content-Disposition", "attachment;filename=" + outputFileName);
+			res.attachment(outputFileName);
+			res.sendFile(outputFileName, {
+				root: __dirname
+			});
+		});
+	}
+});
 
 app.listen((process.env.PORT || 5000), () =>
-  console.log('api listening on port 5000 (probably)!'),
+	console.log('api listening on port 5000 (probably)!'),
 );
